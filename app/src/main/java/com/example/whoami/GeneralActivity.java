3828ca56ace0bf70bcea97c032659cfd68616aeb
Isumairuu <ismail.elfekak@gmail.com>
2021-02-14 00:41:14 +0100

    Update GeneralActivity.java

--- app/src/main/java/com/example/whoami/GeneralActivity.java
+++ app/src/main/java/com/example/whoami/GeneralActivity.java
@@ -15,6 +15,7 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.dnn.Net;
 import org.opencv.core.Point;
@@ -24,6 +25,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -106,6 +108,13 @@ public class GeneralActivity extends CameraActivity implements CameraBridgeViewB
 
         // place the predicted label on the image
         Imgproc.putText(frame, predictedClass, new Point(200, 100), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 121, 0), 3);
+
+        // draw contours
+//        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+//        Imgproc.findContours(frame,contours,new Mat(),Imgproc.RETR_FLOODFILL, Imgproc.CHAIN_APPROX_SIMPLE);
+//        for (int i = 0; i < contours.size(); i++) {
+//            Imgproc.drawContours(frame, contours, i, new Scalar(255, 255, 255), -1);
+//        }
 
         return frame;
     }
